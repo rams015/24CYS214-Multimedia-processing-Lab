a3d235e5d652448cea378841a37af87b4de1de15
rams015 <vrams278@gmail.com>
2025-11-25 23:17:40 +0530

    Add grayscale conversion for an image
    Read an image, convert it to grayscale, and save it.

--- Q1/Desaturation/desaturation.py
+++ Q1/Desaturation/desaturation.py
@@ -1,1 +1,10 @@
+import imageio 
+import numpy as np
 
+img = imageio.imread("D:\VS Code\MMP\image.jpg")
+
+gray = (img[...,0] * 0.299 + img[...,1] * 0.587 + img[...,2] * 0.114)
+
+gray = gray.astype(np.uint8)
+
+imageio.imwrite("D:\VS Code\MMP\greyscale.jpg",gray)
